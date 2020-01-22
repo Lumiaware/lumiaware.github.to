39afced808f095dc1505f8c8e37dae1cfb77a2a3
Lumiaware <60169409+Lumiaware@users.noreply.github.com>
2020-01-22 07:05:52 +0100

    Create sw_cached.js

---
+++ js/sw_cached.js
@@ -0,0 +1,47 @@
+const cacheName = 'v1';
+
+const cacheAssets = [
+    '../index.html',
+    '../about.html',
+    '../css/style.css',
+    '../js/main.js'
+];
+
+self.addEventListener('install', e => {
+    console.log('Service Worker: Installing...');
+    e.waitUntil(
+        caches
+            .open(cacheName)
+            .then(cache => {
+                console.log('Service Worker: Caching Files');
+                cache.addAll(cacheAssets);
+            })
+            .then(() => self.skipWaiting())
+    );
+    console.log('Service Worker: Installed');
+});
+
+self.addEventListener('activate', e => {
+    console.log('Service Worker: Activated');
+    // remove unwanted caches
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== cacheName) {
+                        console.log('Service Worker: Clearing old cache...: ' + cache.toString());
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    );
+});
+
+self.addEventListener('fetch', e => {
+    console.log('Service Worker: Fetching...');
+
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
+});
